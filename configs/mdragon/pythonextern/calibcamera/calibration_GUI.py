f249833ae53188a597e23c6c422625da2c7e5d20
mwork <annhandt09@gmail.com>
2022-01-21 10:50:48 +0700

    update calib

--- configs/mdragon/pythonextern/calibcamera/calibration_GUI.py
+++ configs/mdragon/pythonextern/calibcamera/calibration_GUI.py
@@ -46,7 +46,7 @@ class MyGUI(QMainWindow):
 		self.tot_error = 0
 		self.no_of_columns = 9   #number of columns of your Checkerboard
 		self.no_of_rows = 7  #number of rows of your Checkerboard
-		self.square_size = 20.0 # size of square on the Checkerboard in mm
+		self.square_size = 21.0 # size of square on the Checkerboard in mm
 		self.pixmap = None
 		self.firstPixmap = None # first captured image. to be displayed at the end
 		self.capturing = False
@@ -181,7 +181,8 @@ class MyGUI(QMainWindow):
 		""" captures frame from webcam & tries to detect corners on chess board """
 		ret, frame = self.cap.read() # read frame from webcam
 		if ret: # if frame captured successfully
-			frame_inverted = cv2.flip(frame, 1) # flip frame horizontally
+			#frame_inverted = cv2.flip(frame, 1) # flip frame horizontally
+			frame_inverted = frame
 			if self.detectingCorners: # if detect corners checkbox is checked
 				cornersDetected, corners, imageWithCorners , cornerSubPixs = self.detectCorners(frame_inverted) # detect corners on chess board
 				if cornersDetected: # if corners detected successfully
@@ -200,12 +201,11 @@ class MyGUI(QMainWindow):
 		ret, corners = cv2.findChessboardCorners(gray, (self.no_of_columns,self.no_of_rows ), cv2.CALIB_CB_FAST_CHECK)
 		cornerSubPixs = None
 		if ret:
-			cornerSubPixs = cv2.cornerSubPix(gray, corners, (10, 10), (-1,-1), criteria)
+			cornerSubPixs = cv2.cornerSubPix(gray, corners, (11, 11), (-1,-1), criteria)
 			cv2.drawChessboardCorners(image, (self.no_of_columns,self.no_of_rows ), corners, ret)
 		return ret, corners, image , cornerSubPixs
 
 	def frameWithCornersCaptured(self):
-
 		self.captureClicked() #fix last frame
 		self.toggleConfirmAndIgnoreVisibility(True)
 
@@ -219,18 +219,10 @@ class MyGUI(QMainWindow):
 		if self.confirmedImagesCounter == 1: self.firstPixmap = self.pixmap
 		#if self.confirmedImagesCounter == 3: self.doneButton.show()
 		self.counterLabel.setText('Images taken: '+str(self.confirmedImagesCounter))
-		self.capturedImagePoints[self.confirmedImagesCounter] = self.currentCorners
 		obj_corner = self.cal_real_corner(self.no_of_columns, self.no_of_rows , self.square_size)
 		self.objls.append(obj_corner)
 		self.imgls.append(self.currentcornerSubPixs)
 
-		if self.currentCorners[0,0,0]<self.currentCorners[-1,0,0]:
-			capturedObjectPoints=self.capturedObjectPointsLR
-		else:capturedObjectPoints=self.capturedObjectPointsRL
-		self.objectPoints[self.confirmedImagesCounter] = capturedObjectPoints
-		
-		h = self.computeHomography(self.currentCorners, capturedObjectPoints)
-		self.homographies.append(h)
 		self.toggleConfirmAndIgnoreVisibility(False)
 		self.captureClicked() #continue capturing
 
@@ -270,21 +262,7 @@ class MyGUI(QMainWindow):
 			
 			self.captureClicked() #stop capturing
 			self.mycalib()
-			M=self.buildMmatrix()
-			b=self.getMinimumEigenVector(M)
-			#print(type(b),b)
-			v0, lamda, alpha, betta, gamma, u0, A = self.calculateIntrinsicParam(b)
-			#self.A = A
-			Rt = self.calculateExtrinsicParam(A)
-			#self.Rts = self.calculateAllExtrinsicParam(A, lamda)
-			#self.built_in_calib()
-			self.predicted2D = self.predict2Dpoints(self.objectPoints[1], A, Rt, lamda)
-			self.done_drawPredicted = True
 			self.update()
-			#x = self.MLE(A,Rt)
-			#A_opt = np.array([[x[0],x[1],x[2]],[0,x[3],x[4]],[0,0,1]])
-			#Rt_opt = np.array([[x[5],x[6],x[11]],[x[7],x[8],x[12]],[x[9],x[10],x[13]]])
-			#print('A_opt:',A_opt, 'Rt_opt:',Rt_opt)
 			print('Done :)\nCheck intrinsic.txt & extrinsic.txt & predicted VS actual.txt')
 	
 	def toggleConfirmAndIgnoreVisibility(self, visibility=True):
@@ -309,212 +287,6 @@ class MyGUI(QMainWindow):
 		self.imageLabel.setPixmap(self.pixmap)
 		
 		
-	####################### CALIBRATION STUFF ###########################
-	
-	def computeHomography(self, points2D, points3D):
-		U = self.buildUmatrix(points2D, points3D)
-		return self.getMinimumEigenVector(U)
-	
-	def buildUmatrix(self, points2D, points3D):
-		rows = self.points_in_row * self.points_in_column * 2
-		U = np.zeros((rows, 9))
-		for i in range(len(points2D)):
-			U[i*2,0] = points3D[i][0] #Px
-			U[i*2,1] = points3D[i][1] #Py
-			U[i*2,2] = 1
-			U[i*2,3:6] = 0
-			U[i*2,6:9] = U[i*2,0:3] * -points2D[i,0,0]
-			
-			U[i*2+1,0:3] = 0
-			U[i*2+1,3:6] = U[i*2,0:3]
-			U[i*2+1,6:9] = U[i*2,0:3] * -points2D[i,0,1]
-		return U
-
-	def calculateV(self,h1,h2):
-		v = np.zeros((6,1))
-		v[0,0] = h1[0] * h2[0]
-		v[1,0] = h1[0] * h2[1] + h1[1] * h2[0]
-		v[2,0] = h1[1] * h2[1]
-		v[3,0] = h1[2] * h2[0] + h1[0] * h2[2]
-		v[4,0] = h1[2] * h2[1] + h1[1] * h2[2]
-		v[5,0] = h1[2] * h2[2]
-		return v
-
-	def buildMmatrix(self): # Build the matrix made by homographies to calculate B
-		M = np.zeros((self.confirmedImagesCounter*2, 6))
-		for i in range(len(self.homographies)):
-			h1=self.homographies[i][::3]
-			h2=self.homographies[i][1::3]
-			v12=self.calculateV(h1,h2) #6X1
-			v11=self.calculateV(h1,h1) #6X1
-			v22=self.calculateV(h2,h2) #6X1
-			M[2*i,:]=v12.T # 1X6
-			M[2*i+1,:]=(v11-v22).T # 1X6
-		return M
-
-	def calculateIntrinsicParam(self, b):
-		(B11, B12, B22, B13, B23, B33) = b
-		v0 = (B12*B13-B11*B23) / (B11*B22-B12**2)
-		lamda = B33 - (B13**2 + v0*(B12*B13-B11*B23)) / B11
-		alpha = np.sqrt(lamda / B11)
-		betta = np.sqrt( (lamda*B11) / (B11*B22-B12**2) )
-		gamma = (-B12 * betta * alpha**2) / lamda
-		u0 = (gamma*v0/betta) - (B13*alpha**2/lamda)
-		A = np.array([[alpha,gamma,u0], [0, betta, v0], [0,0,1]])
-		#write intrinsic parameters to file
-		if not os.path.exists('./output'):
-            		os.mkdir('./output')    #make output folder if not exists
-		with open('./output/intrinsic.txt', 'w+') as f:
-			f.write('A=\n{}'.format(A))
-		return v0, lamda, alpha, betta, gamma, u0, A
-
-	def calculateExtrinsicParam(self, A):
-		h1=self.homographies[0][::3] # 1st column of 1st image homography
-		h2=self.homographies[0][1::3] # 2nd column of 1st image homography
-		h3=self.homographies[0][2::3] # 3rd column of 1st image homography
-		A_inv = np.linalg.inv(A)
-		Ah1 = np.dot(A_inv,h2)
-		lamda = 1 / np.sqrt(np.dot(Ah1, Ah1))
-		r1 = lamda * np.dot(A_inv, h1) # 1st column or rotation matrix
-		r2 = lamda * np.dot(A_inv, h2) # 2nd column or rotation matrix
-		r3 = np.cross(r1,r2) # 3rd column or rotation matrix
-		t = lamda * np.dot(A_inv, h3) # translation vector
-		Rt = np.array([r1.T, r2.T, r3.T, t.T]).T
-		#write extrinsic parameters to file
-		if not os.path.exists('./output'):
-            		os.mkdir('./output')    #make output folder if not exists
-		with open('./output/extrinsic.txt', 'w+') as f:
-			f.write('[R|t]=\n{}'.format(Rt))
-		return Rt
-
-	def calculateAllExtrinsicParam(self, A, lamda):
-		Rts = []
-		A_inv = np.linalg.inv(A)
-		for homography in self.homographies:
-			h1=homography[::3]
-			h2=homography[1::3]
-			h3=homography[2::3]
-			r1 = lamda * np.dot(A_inv, h1)
-			r2 = lamda * np.dot(A_inv, h2)
-			r3 = np.cross(r1,r2)
-			t = lamda * np.dot(A_inv, h3)
-			Rt = np.array([r1.T, r2.T, r3.T, t.T]).T
-			Rts.append(Rt)
-		return Rts
-
-	def getMinimumEigenVector(self, U):
-		""" return eigen vector of square matrix U with the minimum eigen value """
-		P = np.dot(U.T, U)
-		w,v = np.linalg.eig(P)
-		i = np.where(w == np.min(w))
-		e1 = v[:,i[0][0]]
-		return e1
-
-	def predict2Dpoints(self, points3D, A, Rt, lamda):
-		Rt = np.delete(Rt, 2, axis=1) # remove r3 column
-		points3D = [[p[0],p[1],1] for p in points3D]
-		imgpoints = np.array([np.squeeze(p) for p in self.capturedImagePoints[1]])
-		imgpoints = np.append(imgpoints, np.ones((imgpoints.shape[0],1)), axis=1) # append 1 to each 2D point
-		pred = []
-		if not os.path.exists('./output'):
-            		os.mkdir('./output')    #make output folder if not exists
-		f = open('./output/predicted VS actual.txt', 'w+')
-		f.write('predicted >> actual')
-		for p3d, p2d in zip(points3D, imgpoints):
-			p3d = np.array(p3d)
-			tmp = np.dot(np.dot(A,Rt), p3d)
-			tmp = tmp / tmp[2]
-			pred.append(tmp)
-			f.write('\n{} , {}  >>  {} , {}'.format(tmp[0],tmp[1],p2d[0],p2d[1]))
-		f.close()
-		return pred
-
-	def save_params(self, save_path='./output/camera_params.xml'):
-		root = ET.Element('root')
-		tree = ET.ElementTree(root)
-		comment = ET.Element('about')
-		comment.set('author', 'annhan91')
-		comment.set('github', 'https://github.com/annhan')
-		root.append(comment)
-		mat_node = ET.Element('camera_matrix')
-		root.append(mat_node)
-		for i, elem in enumerate(self.matrix.flatten()):
-			child = ET.Element('data{}'.format(i))
-			child.text = str(elem)
-			mat_node.append(child)
-
-		new_node = ET.Element('new_camera_matrix')
-		root.append(new_node)
-		for i, elem in enumerate(self.new_camera_matrix.flatten()):
-			child = ET.Element('data{}'.format(i))
-			child.text = str(elem)
-			new_node.append(child)
-
-		dist_node = ET.Element('camera_distortion')
-		root.append(dist_node)
-		for i, elem in enumerate(self.dist.flatten()):
-			child = ET.Element('data{}'.format(i))
-			child.text = str(elem)
-			dist_node.append(child)
-
-		roi_node = ET.Element('roi')
-		root.append(roi_node)
-		for i, elem in enumerate(self.roi):
-			child = ET.Element('data{}'.format(i))
-			child.text = str(elem)
-			roi_node.append(child)
-		tree.write(save_path, 'UTF-8')
-		print("Saved params in {}.".format(save_path))
-
-	def built_in_calib(self): #not used!
-		objpoints = np.array([np.array(p) for k in sorted(self.objectPoints.keys()) for p in self.objectPoints[k]])
-		#objpoints = objpoints.reshape(1,objpoints.shape[0],objpoints.shape[1])
-		objpoints = objpoints.astype('float32')
-		print('objpoints.shape:',objpoints.shape)
-		imgpoints = np.array([np.squeeze(p, axis=0) for k in sorted(self.capturedImagePoints.keys()) for p in self.capturedImagePoints[k]])
-		#imgpoints = imgpoints.reshape(1,imgpoints.shape[0],imgpoints.shape[1])
-		imgpoints = imgpoints.astype('float32')
-		print('imgpoints.shape:',imgpoints.shape)
-		ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera([objpoints], [imgpoints], (640,480), None, None)
-		self.new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (640,480), alpha=1)
-		self.roi = np.array(roi)
-		self.matrix = mtx
-		self.dist = dist
-		self.save_params()
-		print('mtx:',mtx)
-		print('dist:',dist)
-		print('rvecs, tvecs =', rvecs, tvecs)
-
-
-	def loss_function(self, x): #not used!
-		#A, Rt = ARt
-		A = np.array([[x[0],x[1],x[2]],[0.0,x[3],x[4]],[0.0,0.0,1.0]])
-		Rt = np.array([[x[5],x[6],x[11]],[x[7],x[8],x[12]],[x[9],x[10],x[13]]])
-		#Rt = np.delete(Rt, 2, axis=1) # remove r3 column
-		ARt = np.dot(A, Rt)
-		#points3D = np.array([[p[0],p[1],1] for p in points3D])
-		objpoints = np.array([np.array(p) for k in sorted(self.objectPoints.keys()) for p in self.objectPoints[k]])
-		objpoints[:,2] = 1.0
-		imgpoints = np.array([np.squeeze(p) for k in sorted(self.capturedImagePoints.keys()) for p in self.capturedImagePoints[k]])
-		imgpoints = np.append(imgpoints, np.ones((imgpoints.shape[0],1)), axis=1) # append 1 to each 2D point
-		#print('loss_function.points3D.shape:',objpoints.shape)
-		#print('loss_function.points2D.shape:',imgpoints.shape)
-		#print('loss_function.points2D[0]:',imgpoints[0])
-		#loss = 0
-		f = []
-		for p3d, p2d in zip(objpoints, imgpoints):
-			pred = np.dot(ARt, p3d)
-			err = np.dot(p2d-pred, p2d-pred)
-			#loss += err
-			f.append(err)
-		#return loss
-		return f
-
-	def MLE(self, A, Rt): #not used!
-		x = [A[0,0], A[0,1], A[0,2], A[1,1], A[1,2], Rt[0,0], Rt[0,1], Rt[1,0], Rt[1,1], Rt[2,0], Rt[2,1], Rt[0,3], Rt[1,3], Rt[2,3]]
-		sol = optimize.root(self.loss_function, x, jac=False, method='lm')
-		return sol.x
-
 	########################################################
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
