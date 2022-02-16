4bbc53d2f3f3b6983b09648f13127b66a255b9df
mwork <annhandt09@gmail.com>
2022-02-16 14:29:42 +0700

    Update CoffeeState.py

--- configs/mdragon/pythonextern/CoffeMachine/CoffeeState.py
+++ configs/mdragon/pythonextern/CoffeMachine/CoffeeState.py
@@ -2,11 +2,13 @@
 import time
 
 
+
 class Item:
-    def __init__(self, name, price, stock):
+    def __init__(self, name, price, stock ,controlfile):
         self.name = name
         self.price = price
         self.stock = stock
+        self.controlfile = controlfile
 
     def updateStock(self, stock):
         self.stock = stock
@@ -18,15 +20,28 @@ class Item:
         self.stock -= 1 # else stock of item decreases by 1
 
 class State:
+    def __init__(self):
+        self.myRobot=None 
+        self.text = "Ss" 
+        self.mprint("Init STATE")
+
     def scan(self):
         pass
-    def mprint(self,msg):
 
+    def mprint(self,msg):
         print(msg,flush=True)
 
+    def moveRobot(self,msg):
+        pass
+
+class RobotControl(State):
+    def __init__(self):
+        self.myRobot=None 
+        self.text = "Ss" 
+        self.mprint("Init STATE")   
+
 class WaitChooseItemState(State):
-  
-    """constructor for AM state class"""
+
     def __init__(self, machine):      
         self.machine = machine
 
@@ -54,16 +69,17 @@ class WaitChooseItemState(State):
         return ret
 
 class showItemsState(State):
-    def __init__(self, machine):      
+    def __init__(self, machine):  
+        State.__init__(self)    
         self.machine = machine
 
     def checkAndChangeState(self):
         self.mprint("Switching to showItemsState")
         self.mprint('\nitems available \n***************')
-
-        for item in self.machine.items: # for each item in this vending machine
-            if item.stock == 0: # if the stock of this item is 0
-                self.machine.items.remove(item) # remove this item from being displayed
+        #remove item,which have stock is 0
+        #for item in self.machine.items: # for each item in this vending machine
+        #    if item.stock == 0: # if the stock of this item is 0
+        #        self.machine.items.remove(item) # remove this item from being displayed
         for item in self.machine.items:
             self.mprint(item.name + " Gia: "+ str(item.price) + " + SL :" + str(item.stock)) # otherwise self.mprint this item and show its price
 
@@ -73,60 +89,75 @@ class showItemsState(State):
 
 class waitMoneyToBuyState(State):
   
-    """constructor for AM state class"""
     def __init__(self, machine):      
         self.machine = machine
 
     def checkAndChangeState(self):
         price = self.machine.item.price
-        if self.machine.amount < price:
-            self.machine.amount = self.machine.amount + float(input('insert ' + str(price - self.machine.amount) + ': '))
+        if self.machine.moneyGet < price:
+            self.machine.moneyGet = self.machine.moneyGet + float(input('insert ' + str(price - self.machine.moneyGet) + ': '))
         else:
             self.machine.state = self.machine.buyItemState
 
 class buyItemState(State):
-  
-    """constructor for AM state class"""
     def __init__(self, machine):      
         self.machine = machine
 
     def checkAndChangeState(self):
-        if self.machine.amount < self.machine.item.price:
+        if self.machine.moneyGet < self.machine.item.price:
             self.mprint('You can\'t buy this item. Insert more coins.') # then obvs you cant buy this item
+            self.machine.state = self.machine.waitMoneyToBuyState
         else:
-            self.machine.amount -= self.machine.item.price # subtract item price from available cash
+            self.machine.moneyGet -= self.machine.item.price # subtract item price from available cash
             self.machine.item.buyFromStock() # call this function to decrease the item inventory by 1
             # (what if we buy more than one?)
             self.mprint('You got ' +self.machine.item.name)
-            self.mprint('Cash remaining: ' + str(self.machine.amount))
-        self.machine.state = self.machine.checkRefundState
+            self.mprint('Cash remaining: ' + str(self.machine.moneyGet))
+            self.machine.state = self.machine.takeCoffeeState
 
-class checkRefundState(State):
-  
-    """constructor for AM state class"""
+class takeCoffeeState(State):
     def __init__(self, machine):      
         self.machine = machine
 
     def checkAndChangeState(self):
-        if self.machine.amount > 0:
-            self.mprint(str(self.machine.amount) + " refunded.")
-            self.machine.amount = 0
-        self.mprint('Thank you, have a nice day!\n')
+        gcode = []
+        self.mprint('Control Robot to '+str(self.machine.item.controlfile))
+        f = open("./gcode/" + self.machine.item.controlfile, "r")
+        for line in f.readlines():
+            gcode.append(line)
+        self.mprint(gcode)
+        f.close()
+        #for x in gcode:
+        #    self.mprint(x)
+        self.machine.state = self.machine.checkRefundState
 
+class checkRefundState(State):
+    def __init__(self, machine):      
+        self.machine = machine
+
+    def checkAndChangeState(self):
         self.mprint("Switching to checkRefundState")
+        if self.machine.moneyGet > 0:
+            self.mprint(str(self.machine.moneyGet) + " refunded.")
+            self.machine.moneyGet = 0
+        self.mprint('Thank you, have a nice day!\n')
         self.machine.state = self.machine.showItemsState
 
 class Machine:
  
-    def __init__(self):
-        self.amount = 0
+    def __init__(self,robot):
+        self.myrobot= robot
+        self.moneyGet = 0
         self.items = [] # all items contained in this list right here
-        self.item=None         
+        self.item=None 
+        self.timeout = 10        
         self.showItemsState = showItemsState(self)
         self.WaitChooseItemState = WaitChooseItemState(self)
         self.waitMoneyToBuyState = waitMoneyToBuyState(self)
-        self.checkRefundState = checkRefundState(self)
         self.buyItemState = buyItemState(self)
+        self.takeCoffeeState = takeCoffeeState(self)
+        self.checkRefundState = checkRefundState(self)
+        
         self.state = self.showItemsState
 
     def run(self):
@@ -136,18 +167,19 @@ class Machine:
         self.state.scan()
 
     def addItem(self, item):
-        self.items.append(item) # method to add item to vending machine
+        self.items.append(item) 
 
 
 def vend():
-
-    machine = Machine()
-    item1 = Item('choc',  1.5,  0)
-    item2 = Item('pop', 1.75,  1)
-    item3 = Item('chips',  2.0,  3)
-    item4 = Item('gum',  0.50, 1)
-    item5 = Item('mints',0.75,  3)
-    item6 = Item('milkshake',1.2, 5 )
+    robot=RobotControl()
+    machine = Machine(robot)
+    #              name,giá,số lượng
+    item1 = Item('caffe d',      1.5,    88, "caffeden.ngc" )
+    item2 = Item('caffe s',      1.75,   1, "caffesua.ngc")
+    item3 = Item('12'    ,       2.0,    3, "nuocngot.ngc")
+    item4 = Item('23'    ,       0.50,   1,  "nuocngot.ngc")
+    item5 = Item('45'    ,       0.75,   3,  "nuocngot.ngc")
+    item6 = Item('milkshake',    1.2,    5, "nuocngot.ngc")
     machine.addItem(item1)
     machine.addItem(item2)
     machine.addItem(item3)
